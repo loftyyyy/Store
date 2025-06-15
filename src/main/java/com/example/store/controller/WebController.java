db302290b699eee6909c2a31a0b2d4d58aa20228
loftyyyy <lofty08.business@gmail.com>
2025-06-16 00:13:29 +0800

    Fixed the wokring tree

---
+++ src/main/java/com/example/store/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.example.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello"; // Returns the index.html file located in src/main/resources/templates
+    }
+
+
+}
